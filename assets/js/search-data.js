6c0fd758133a1677dc9549aca1575eebabec111a
eugeniopetrovich <eugenio.petrovich@gmail.com>
2026-01-21 16:17:04 +0000

    Deploying to gh-pages from @ eugeniopetrovich/eugeniopetrovich.github.io@d8c5b1f4b85e22c8f8b965c41d05fad7ed66feea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-poster-at-epsa25",
+        },{id: "post-unito-working-group-on-open-science",
+        
+          title: "UniTO Working Group on Open Science",
+        
+        description: "Official appointment",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/Open_Science/";
+          
+        },
+      },{id: "post-poster-at-epsa25",
         
           title: "Poster at EPSA25",
         
